b6926c8f9eb710d56fb95d234c869e75ed801a8d
Pavani Chitla <pavanichitla24@gmail.com>
2024-04-14 07:31:45 +0000

    commit

--- models/chinook-data/customers.sql
+++ models/chinook-data/customers.sql
@@ -1,9 +1,8 @@
 
-{{
-    config(
-        materialized='table'
-    )
-}}
-select customerid as cust_id,
-supportrepid as support_rep_id 
-from DBT_LEARNING.CHINOOK_DATA.CUSTOMERS
+{{ 
+    config(materialized="table")
+    
+     }}
+
+select customerid as cust_id, supportrepid as support_rep_id
+from dbt_learning.chinook_data.customers
---
+++ models/chinook-data/customers1.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select customerid as cust_id, supportrepid as support_rep_id
+from {{ source('chinook_data','customers')}}
