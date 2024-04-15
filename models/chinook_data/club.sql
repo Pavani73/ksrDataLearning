f394f08d8b1a7ac37b57a64f052e5f2ad6016053
Pavani Chitla <pavanichitla24@gmail.com>
2024-04-15 03:47:12 +0000

    invoice

--- models/chinook_data/club.sql
+++
@@ -1,9 +0,0 @@
-with
-customers as (select * from {{ ref('customers') }}),
-genre as (select * from {{ ref('genre') }}),
-
-student as (select * from {{ ref('student1') }}
-
-select * from customers 
-inner join 
-select * from student
---
+++ models/chinook_data/customer_invoice_info.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with
+customers as (select * from {{ ref('customers') }}),
+invoice as (select * from {{ ref('invoice') }}),
+employee as (select * from {{ ref('employee') }})
+
+Select c.cust_id, i.invoice_id,e.emp_name
+
+from customer as c
+
+left join invoice as i on i.cust_id = c.cust_id
+
+left join employee as e on e.emp_id = c.support_rep_id
