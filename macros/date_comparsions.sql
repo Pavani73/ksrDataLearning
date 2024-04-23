392fb45ed1abd46dac3a90994547bcdcdcb3bf2a
Pavani Chitla <pavanichitla24@gmail.com>
2024-04-23 04:47:51 +0000

    macro

---
+++ macros/date_comparsions.sql
@@ -0,0 +1,17 @@
+{% macro date_comparsions(date1, date2) %}
+
+CASE
+
+WHEN {{date1}} < {{date2}} then {{date2}}
+
+WHEN {{date1}} > {{date2}} then {{date1}}
+
+ELSE
+
+'9999-12-31'
+
+END as date_result
+
+{% endmacro %}
+
+
